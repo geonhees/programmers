8d700fa50b99741cc1afd76884534e07852a70d4
geonhees <ghkim1130one@gmail.com>
2026-08-31 17:19:05 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.21 ms, Memory: 46.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public string solution(string s) {
+        string answer = "";
+        if(s.Length%2 == 0)
+        {
+            answer = s[s.Length/2-1].ToString();
+            answer += s[s.Length/2].ToString();
+        }
+        else
+        {
+            answer = s[s.Length/2].ToString();
+        }
+        return answer;
+    }
+}
